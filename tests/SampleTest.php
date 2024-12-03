499a79a9370a3bb3ff65b6cf45e85f370b3cc219
marwensouissi <marwen.souissi00@gmail.com>
2024-12-03 23:35:56 +0100

    PHP

---
+++ tests/SampleTest.php
@@ -0,0 +1,11 @@
+// tests/SampleTest.php
+<?php
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
